0e2ca3a628e366209a8ae41bf498b870d8f8bad9
Tigran Naghdalyan <naghdalyan@Tigrans-MacBook-Pro.local>
2025-05-04 14:10:24 +0400

    chore: update error message

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,10 +1,9 @@
 export const DEFAULT_PAGE = 1;
-export const DEFAULT_PER_PAGE = 20;
 export const ITEMS_PER_PAGE = 30;
 export const INTERNAL_SERVER_ERROR = 500;
 
 export const ERROR_MESSAGES = {
-  DEFAULT_ERROR: 'An error occurred',
+  DEFAULT_ERROR: 'Something went wrong',
   UNEXPECTED_ERROR: 'An unexpected error occurred',
   API_ERROR: 'API Error:',
 };
