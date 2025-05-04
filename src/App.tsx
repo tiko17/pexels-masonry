bc1673fb6523f0c3300bc14b1d14cd3351a2dc1c
Tigran Naghdalyan <naghdalyan@Tigrans-MacBook-Pro.local>
2025-05-04 14:09:25 +0400

    feat: add app routes: home page, photo detail

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import PhotoDetails from './pages/PhotoDetails';
+import ErrorBoundary from './components/ErrorBoundary';
 
 const App: React.FC = () => {
   return (
-    <Router>
-      <Routes>
-        {/* Routes will be added here */}
-      </Routes>
-    </Router>
+    <ErrorBoundary>
+      <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/photo/:id" element={<PhotoDetails />} />
+        </Routes>
+      </Router>
+    </ErrorBoundary>
   );
 };
 
-export default App; 
+export default App;
