4e2d7deb32073b17cd4986ace54cb8715ddd347b
Tigran Naghdalyan <naghdalyan@Tigrans-MacBook-Pro.local>
2025-05-04 21:21:01 +0400

    feat: optimize App.tsx for performance

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,40 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import PhotoDetails from './pages/PhotoDetails';
+import { ThemeProvider, StyleSheetManager } from 'styled-components';
+import { theme } from './styles';
 import ErrorBoundary from './components/ErrorBoundary';
+import LoadingState from './components/LoadingState';
+import { LoadingContainer } from './styles/components/app.styles';
+
+// Lazy load route components
+const Home = lazy(() => import('./pages/Home'));
+const PhotoDetails = lazy(() => import('./pages/PhotoDetails'));
+
+// Function to filter styled-component props
+const shouldForwardProp = (prop: string) => !prop.startsWith('$');
 
 const App: React.FC = () => {
   return (
-    <ErrorBoundary>
-      <Router>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/photo/:id" element={<PhotoDetails />} />
-        </Routes>
-      </Router>
-    </ErrorBoundary>
+    <StyleSheetManager shouldForwardProp={shouldForwardProp}>
+      <ThemeProvider theme={theme}>
+        <ErrorBoundary>
+          <Router>
+            <Suspense 
+              fallback={
+                <LoadingContainer>
+                  <LoadingState />
+                </LoadingContainer>
+              }
+            >
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/photo/:id" element={<PhotoDetails />} />
+              </Routes>
+            </Suspense>
+          </Router>
+        </ErrorBoundary>
+      </ThemeProvider>
+    </StyleSheetManager>
   );
 };
 
