171c0bdb2172a35b5143b77455b568f50bd62b97
Tigran Naghdalyan <naghdalyan@Tigrans-MacBook-Pro.local>
2025-05-05 15:00:09 +0400

    feat: add tests setup file

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,3 +3,29 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+// Mock IntersectionObserver if needed
+class MockIntersectionObserver {
+  observe = jest.fn();
+  unobserve = jest.fn();
+  disconnect = jest.fn();
+}
+
+Object.defineProperty(window, 'IntersectionObserver', {
+  writable: true,
+  configurable: true,
+  value: MockIntersectionObserver
+});
+
+// Mock ResizeObserver if needed
+class MockResizeObserver {
+  observe = jest.fn();
+  unobserve = jest.fn();
+  disconnect = jest.fn();
+}
+
+Object.defineProperty(window, 'ResizeObserver', {
+  writable: true,
+  configurable: true,
+  value: MockResizeObserver
+});
